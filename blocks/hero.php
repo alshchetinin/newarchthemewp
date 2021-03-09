152f4306a6c2a5db293f288140aba6fb6167a969
alshchetinin <alexsander.qw@gmail.com>
2021-03-09 16:40:55 +0300

    add subscripe popup

---
+++ blocks/hero.php
@@ -0,0 +1,121 @@
+<section class="hero">
+  <div class="wrapper">
+    <div class="row">
+      <div class="hero-text">
+        <h1>
+          <span>Привет. Мы — диджитал-компания с&nbsp;экспертизой в недвижимости.</span>
+          <span>Создаем <span class="slider-dots " style="--active-slider-color: linear-gradient(203.52deg, #8E54E9 0%, #56B4D3 80.7%)">сайты</span> и <span class="slider-dots " style="--active-slider-color: linear-gradient(0deg, #FFFFFF, #FFFFFF), linear-gradient(89.99deg, #FFFFFF -120.05%, #8E8F8F 99.99%)">брендинг,</span> запускаем <span class="slider-dots " style="--active-slider-color: linear-gradient(90deg, #FFD43C 0%, #F15F79 98.96%)">перфоманс-маркетинг </span>
+            и&nbsp;<span class="slider-dots " style="--active-slider-color: linear-gradient(90deg, #4ECDC2 0%, #24827C 100%)">контент-маркетинг,</span> проводим <span class="slider-dots " style="--active-slider-color: linear-gradient(90deg, #F857A6 0%, #FF5858 100%)">комплексный аудит.</span></span>
+        </h1>
+      </div>
+    </div>
+  </div>
+
+  <div class="hero-slider">
+    <div class="hero-slider__slider-new">
+      <img src="<?php echo get_template_directory_uri()?>/img/site-slider-bg.png" alt="">
+    </div>
+    <div class="hero-slider__slider-new">
+      <img src="<?php echo get_template_directory_uri()?>/img/branding-slider-bg.png" alt="">
+    </div>
+    <div class="hero-slider__slider-new">
+      <img src="<?php echo get_template_directory_uri()?>/img/performans-slider-bg.png" alt="">
+    </div>
+    <div class="hero-slider__slider-new">
+      <img src="<?php echo get_template_directory_uri()?>/img/content-slider-bg.png" alt="">
+    </div>
+    <div class="hero-slider__slider-new">
+      <img src="<?php echo get_template_directory_uri()?>/img/audit-sldier-bg.png" alt="">
+    </div>
+
+
+
+  </div>
+</section>
+
+<script>
+  $('.hero-slider').slick({
+    infinite: true,
+    pauseOnHover: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 4500,
+    infinite: true,
+    cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+    arrows: false,
+    // dots: true,
+    // appendDots: $('.main-slider-dots__row')
+  });
+</script>
+
+
+
+    <!-- <div class="hero-slider__slide">
+      <div class="blur_1" style="background: #00BFFE;"></div>
+      <div class="slide-img-wrapper">
+        <div class="wrapper">
+          <div class="row">
+            <img src="<?php echo get_template_directory_uri()?>/img/pic-site.png" alt="" class="hero-slider__pic">      
+          </div>
+      </div>
+      
+      </div>
+      
+      <div class="blur_2" style="background: #00BFFE;"></div>
+
+
+    </div>
+    <div class="hero-slider__slide">
+      <div class="blur_1" style="background: linear-gradient(90deg, #505152 0%, #8E8F8F 100%)"></div>
+      <div class="slide-img-wrapper">
+        <div class="wrapper">
+          <div class="row">
+            <img src="<?php echo get_template_directory_uri()?>/img/pic-site.png" alt="" class="hero-slider__pic">      
+          </div>
+      </div>
+      
+      <div class="blur_2" style="background: linear-gradient(90deg, #505152 0%, #8E8F8F 100%)"></div>
+
+
+    </div>
+    <div class="hero-slider__slide">
+      <div class="blur_1" style="background: linear-gradient(0deg, #FFE07C, #FFE07C)"></div>
+      <div class="slide-img-wrapper">
+        <div class="wrapper">
+          <div class="row">
+            <img src="<?php echo get_template_directory_uri()?>/img/pic-site.png" alt="" class="hero-slider__pic">      
+          </div>
+      </div>
+      
+      <div class="blur_2" style="background: linear-gradient(0deg, #FFE07C, #FFE07C)"></div>
+
+
+    </div>
+    <div class="hero-slider__slide">
+      <div class="blur_1" style="background: linear-gradient(90deg, #BFE85B 0%, #2D9D84 98.96%)"></div>
+      <div class="slide-img-wrapper">
+        <div class="wrapper">
+          <div class="row">
+            <img src="<?php echo get_template_directory_uri()?>/img/pic-site.png" alt="" class="hero-slider__pic">      
+          </div>
+      </div>
+      
+      <div class="blur_2" style="background: linear-gradient(90deg, #BFE85B 0%, #2D9D84 98.96%)"></div>
+
+
+    </div>
+    <div class="hero-slider__slide">
+      <div class="blur_1" style="background:  #FF466E;"></div>
+      <div class="slide-img-wrapper">
+        <div class="wrapper">
+          <div class="row">
+            <img src="<?php echo get_template_directory_uri()?>/img/pic-site.png" alt="" class="hero-slider__pic">      
+          </div>
+      </div>
+      
+      </div>
+      
+      <div class="blur_2" style="background:  #FF466E;"></div>
+
+
+    </div> -->
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<?php echo get_template_part('/blocks/main-slider') ?>
+<?php echo get_template_part('/blocks/hero') ?>
 <?php echo get_template_part('/blocks/lead-section') ?>
 <?php echo get_template_part('/blocks/service-section') ?>
 
