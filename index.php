4dcd5ffe213a78c25b563d4be54506fd39154a4b
alshchetinin <alexsander.qw@gmail.com>
2021-03-16 10:19:55 +0300

    chane project page

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 <?php echo get_template_part('/blocks/hero') ?>
 <?php echo get_template_part('/blocks/main-project') ?>
-<!-- <?php echo get_template_part('/blocks/lead-section') ?> -->
 <?php echo get_template_part('/blocks/service-section') ?>
 
 <section id="headline-section" class="g-block">
@@ -10,7 +9,7 @@
       <div class="headline-section">
          <div class="headline-section__title">
             <h2 class="font-super-gigant">
-            <?php the_field('title-text-and-ilustration', 'option')?>
+               <?php the_field('title-text-and-ilustration', 'option') ?>
             </h2>
 
          </div>
@@ -34,7 +33,7 @@
       <div class="row">
          <!-- begin text-service -->
          <div class="text-service">
-            <h2 class="font-super-gigant"><?php the_field('title-block-gradient', 'option')?></h2>
+            <h2 class="font-super-gigant"><?php the_field('title-block-gradient', 'option') ?></h2>
 
             <ul class="text-service__wrap">
                <?php $args = array(
@@ -47,68 +46,82 @@
                if ($property->have_posts()) : ?>
                   <?php while ($property->have_posts()) :
                      $property->the_post(); ?>
-                     <li><a href="<?php the_permalink()?>" class="font-h2" style="background-image: linear-gradient(<?php the_field('gradient') ?>);"><?php the_title() ?></a></li>         
-      <?php endwhile; ?>
-   <?php endif;
+                     <li><a href="<?php the_permalink() ?>" class="font-h2" style="background-image: linear-gradient(<?php the_field('gradient') ?>);"><?php the_title() ?></a></li>
+                  <?php endwhile; ?>
+               <?php endif;
                wp_reset_postdata();
-   ?>
-   </ul>
+               ?>
+            </ul>
 
 
+         </div>
+         <!-- end text-service -->
       </div>
-      <!-- end text-service -->
-   </div>
    </div>
 </section>
 
 
 <script>
-   $(function () {
+   $(function() {
       var controller = new ScrollMagic.Controller();
-   var scrollDuration = 3 * $('.main-project-list-item').height();
-   var transformXimg = 3 * $('.main-project-container__img').width();
-   var tl = gsap.timeline()
-   var tween = gsap.to(".main-project-container__img", { x: -transformXimg, ease: "linear" });
-   var tweenColorProject1 = gsap.to(".main-project-container__set", { backgroundColor: "#FACB52", ease: "linear" });
-   var tweenColorProject2 = gsap.to(".main-project-container__set", { backgroundColor: "#CFA3DD", ease: "linear" });
-   var tweenColorProject3 = gsap.to(".main-project-container__set", { backgroundColor: "#A5E6FD", ease: "linear" });
-   
-   var scene = new ScrollMagic.Scene({
-      triggerElement: "#main-project",
-      triggerHook: "onLeave",
-      duration: scrollDuration,
-   })
-      .setTween(tween)
-      .setPin(".main-project-container", { pushfollowers: true })
-      // .addIndicators({ name: `2 (duration: ${scrollDuration})` })
-      .addTo(controller);
-      
-   var scentColorProject1 = new ScrollMagic.Scene({
-      triggerElement: "#project1",
-      triggerHook: "onLeave",
-      duration: $('.main-project-list-item').height(),
-   })
-      .setTween(tweenColorProject1)
-      // .addIndicators({ name: `2 (duration: ${scrollDuration})` })
-      .addTo(controller);
+      var scrollDuration = 3 * $('.main-project-list-item').height();
+      var transformXimg = 3 * $('.main-project-container__img').width();
+      var tl = gsap.timeline()
+      var tween = gsap.to(".main-project-container__img", {
+         x: -transformXimg,
+         ease: "linear"
+      });
+      var tweenColorProject1 = gsap.to(".main-project-container__set", {
+         backgroundColor: "#FACB52",
+         ease: "linear"
+      });
+      var tweenColorProject2 = gsap.to(".main-project-container__set", {
+         backgroundColor: "#CFA3DD",
+         ease: "linear"
+      });
+      var tweenColorProject3 = gsap.to(".main-project-container__set", {
+         backgroundColor: "#A5E6FD",
+         ease: "linear"
+      });
+
+      var scene = new ScrollMagic.Scene({
+            triggerElement: "#main-project",
+            triggerHook: "onLeave",
+            duration: scrollDuration,
+         })
+         .setTween(tween)
+         .setPin(".main-project-container", {
+            pushfollowers: true
+         })
+         // .addIndicators({ name: `2 (duration: ${scrollDuration})` })
+         .addTo(controller);
+
+      var scentColorProject1 = new ScrollMagic.Scene({
+            triggerElement: "#project1",
+            triggerHook: "onLeave",
+            duration: $('.main-project-list-item').height(),
+         })
+         .setTween(tweenColorProject1)
+         // .addIndicators({ name: `2 (duration: ${scrollDuration})` })
+         .addTo(controller);
 
       var scentColorProject2 = new ScrollMagic.Scene({
-         triggerElement: "#project2",
-         triggerHook: "onLeave",
-         duration: $('.main-project-list-item').height(),
-      })
+            triggerElement: "#project2",
+            triggerHook: "onLeave",
+            duration: $('.main-project-list-item').height(),
+         })
          .setTween(tweenColorProject2)
          // .addIndicators({ name: `2 (duration: ${scrollDuration})` })
-         .addTo(controller);    
-         var scentColorProject3 = new ScrollMagic.Scene({
+         .addTo(controller);
+      var scentColorProject3 = new ScrollMagic.Scene({
             triggerElement: "#project3",
             triggerHook: "onLeave",
             duration: $('.main-project-list-item').height(),
          })
-            .setTween(tweenColorProject3)
-            // .addIndicators({ name: `2 (duration: ${scrollDuration})` })
-            .addTo(controller);              
+         .setTween(tweenColorProject3)
+         // .addIndicators({ name: `2 (duration: ${scrollDuration})` })
+         .addTo(controller);
 
-   });
-</script
-<?php get_footer(); ?>
+   }); 
+   </script>
+   <?php get_footer(); ?>
--- projects.php
+++ projects.php
@@ -10,51 +10,79 @@ Template Post Type: page
    <div class="wrapper">
       <div class="row">
          <div class="page-title">
-            <h1 class="very-big-text"><?php the_title() ?></h1>
+            <h1 class="font-super-gigant"><?php the_title() ?></h1>
+            <div class="fillter">
+            <?php
+            $terms_product_cat = get_terms(array(
+               'post_type' => 'product',
+               'hide_empty' => true,
+               'orderby' => 'id',
+               'taxonomy' => 'category'
+            ));
+            if (!empty($terms_product_cat)) {
+            ?>
+               <?php foreach ($terms_product_cat as $key => $product_cat) { ?>
+                  <a class="fillter__item <?php if ($key == 0) echo 'fillter__item-active'; ?>" href="#<?php echo $product_cat->slug; ?>" data-tab="<?php echo $product_cat->slug; ?>"><?php echo $product_cat->name; ?></a>
+               <?php } ?>
+
+
+         </div>
          </div>
       </div>
    </div>
 </section>
 <section>
-   <div class="projects">
-      <?php $args = array(
-         'post_type' => 'project',
-         'posts_per_page' => -1,
-      );
-      $property = new WP_Query($args); // дальше - loop
-      if ($property->have_posts()) : ?>
-         <?php while ($property->have_posts()) :
-            $property->the_post();
-            $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
-            $category = get_the_category();
-         ?>
-            <div class="project-item" style="background-image:url(<?php echo $url ?>)">
-            <div class="project-item__overlay"></div>
-            
-               <a href="<?php the_permalink() ?>"></a>
-               <div class="project-item__content">
-                  <div class="project-item__title">
-                     <?php the_title() ?>
+
+   <div class="">
+      <?php foreach ($terms_product_cat as $key => $product_cat) { ?>
+
+         <div class="project_content_tab projects <?php if ($key == 0) echo 'project_content_tab-active'; ?>" id="<?php echo $product_cat->slug; ?>" data-tab="<?php echo $product_cat->slug; ?>">
+            <?php
+                  $mypost = array('post_type' => 'project', 'numberposts' => -1, 'category' => $product_cat->term_id);
+                  $products = get_posts($mypost);
+            ?>
+            <?php foreach ($products as $post) {
+                     setup_postdata($post);
+                     $url = get_field('kartinka');
+                     $category = get_the_category();
+                     $darktext = get_field('dark-text');
+                     $darkclass = '';
+                     if ($darktext){
+                        $darkclass = 'project-item_corlordark';
+                     }
+
+            ?>
+               <div class="project-item <?php echo $darkclass?> " style="background-image:url(<?php echo $url ?>)">                  
+
+                  <a href="<?php the_permalink() ?>"></a>
+                  <div class="project-item__content">
+                     <div>
+                        <div class="project-item__title">
+                           <?php the_field('zagolovok') ?>
+                        </div>
+                        <div class="project-item__client">
+                        <?php the_field('klient') ?>
+                        </div>
+                     </div>
+                     <div class="">
+                        <div class="project-item__factoid">
+                           <h3><?php the_field('czifra_dlya_faktoida') ?></h3>
+                           <p><?php the_field('tekst_dlya_faktoida') ?></p>
+                        </div>
+                     </div>
                   </div>
-                  <div class="project-item__description">
-                     <?php the_excerpt() ?>
-                  </div>
-
                </div>
-               <div class="project-item__tag">
-                  <span><?php echo $category[0]->name; ?></span>
-
-               </div>
-
-            </div>
-         <?php endwhile; ?>
-      <?php endif;
-      wp_reset_postdata();
-      ?>
-
-
+            <?php }
+                  wp_reset_postdata(); ?>
+         </div>
+      <?php } ?>
    </div>
+<?php } ?>
+</div>
+</div>
 </section>
+
+
 <div class="site-content page">
    <?php the_post();
    the_content(); ?>
